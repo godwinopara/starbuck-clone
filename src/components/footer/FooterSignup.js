f8e4428bcc07096615633ae955c9d7831587a99a
Godwin Opara <godwinopara62@yahoo.com>
2021-06-21 15:23:35 +0400

    finished styling the sign up page

--- src/components/footer/FooterSignup.js
+++ src/components/footer/FooterSignup.js
@@ -2,17 +2,24 @@ import React from "react";
 
 function FooterSignup() {
 	return (
-		<footer className="container footer-signup">
-			<div className="find-store-btn">
-				<button>Find a store</button>
+		<footer className="footer-signup">
+			<div className="footer-sign-up-wrapper container">
+				<div className="flex">
+					<div className="find-store-btn">
+						<button>Find a store</button>
+					</div>
+					<ul className="footer-sign-up-links">
+						<li>Responsibility</li>
+						<li>Web Accessibility</li>
+						<li>Privacy Policy</li>
+						<li>Terms of Use</li>
+						<li>Cookie Preferences</li>
+					</ul>
+				</div>
+				<div className="copy-right">
+					<p>&copy;2021 Starbucks</p>
+				</div>
 			</div>
-			<ul className="footer-links">
-				<li>Responsibility</li>
-				<li>Web Accessibility</li>
-				<li>Privacy Policy</li>
-				<li>Terms of Use</li>
-				<li>Cookie Preferences</li>
-			</ul>
 		</footer>
 	);
 }
